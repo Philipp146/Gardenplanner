6ff35cfd59d622419712c91923f27016206633e3
philipp <duemleinp@googlemail.com>
2018-01-11 11:32:19 +0100

    Initial Commit

---
+++ Gardenplanner/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gardenplanner
+//
+//  Created by Philipp on 11.01.18.
+//  Copyright © 2018 Philipp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
